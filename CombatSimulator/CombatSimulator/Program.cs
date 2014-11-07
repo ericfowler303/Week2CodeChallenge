bb5e0336f7d2ac33fb9a47aedb33cec943dfe937
Eric Fowler <ericfowler303@gmail.com>
2014-11-07 16:24:12 -0700

    Add Bill Lumbergh Quotes

--- CombatSimulator/CombatSimulator/Program.cs
+++ CombatSimulator/CombatSimulator/Program.cs
@@ -8,6 +8,13 @@ namespace CombatSimulator
 {
     class Program
     {
+         static List<string> billQuote = new List<string>() {
+        "Umm yea...I'm gonna need those TPS Reports....ASAP...",
+        "Umm yea...Did you read the memo about the TPS Reports",
+        "Ah, ah, I almost forgot... I'm also going to need you to go ahead and come in on Sunday, too. We, uhhh, lost some people this week and we sorta need to play catch-up. Mmmmmkay? Thaaaaaanks.",
+        "....So, if you could do that, that would be great...",
+        "Hello Peter, what's happening? Listen, are you gonna have those TPS reports for us this afternoon?",
+        "Oh, and next Friday is Hawaiian shirt day.......So, you know, if you want to you can go ahead and wear a Hawaiian shirt and jeans."};
         static bool smashedPrinter = false;
         static bool accountingVirusRunning = false;
         static double enemyMoney = 200.0;
@@ -43,7 +50,13 @@ namespace CombatSimulator
             // Play the game
             while (isPlaying)
             {
-                
+                // Get the user weapon choice
+                string userChoice = Console.ReadLine();
+                CalcPlayerDamage(userChoice);
+              
+
+                // Display results of attack
+                PrintAttackScreen();
             }
 
             // End of game
@@ -69,32 +82,32 @@ namespace CombatSimulator
             }
         }
 
-        static double CalcPlayerDamage(int weaponChoice)
+        static double CalcPlayerDamage(string weaponChoice)
         {
             switch (weaponChoice)
             {
                 // Tetris at Work
-                case 1:
+                case "1":
                     if (rng.NextDouble() >= 0.3) { return rng.Next(20, 36); }
                     break;
 
                 // Burn TPS reports
-                case 2:
+                case "2":
                     return rng.Next(10, 16);
 
                 // Grab a beer at Chotchkie's with Joanna
-                case 3:
+                case "3":
                     // update $ due to healing
                     yourMoney += rng.Next(10, 21);
                     // Healing doesn't hurt the enemy
                     return 0;
 
                 // smashed printer
-                case 4:
+                case "4":
                     return 0;
 
                 // accounting virus
-                case 5:
+                case "5":
                     return 0;
             }
 
@@ -108,7 +121,21 @@ namespace CombatSimulator
         {
             Console.Clear();
             Console.WriteLine("Office Space\n");
+
             // Print enemy attack results
+            double enemyDamage = CalcEnemyDamage();
+            yourMoney -= enemyDamage;
+            if (enemyDamage != 0)
+            {
+                // Enemy hit
+                Console.WriteLine(billQuote[rng.Next(billQuote.Count)]); // Print random Bill Lubergh Quote
+                Console.WriteLine("Bill Lumbergh hit you with " + enemyDamage + "");
+            }
+            else
+            {
+                // Enemy missed
+                Console.WriteLine("Bill Lumbergh somehow missed your desk in his rounds today");
+            }
             // Print your attack
             // Keep the attack screen on screen for 5 seconds before going back to the main menu
             System.Threading.Thread.Sleep(5000);

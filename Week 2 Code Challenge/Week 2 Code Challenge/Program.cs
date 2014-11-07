d4df66c8d6952ae67bb387ff1dba133b43279beb
Eric Fowler <ericfowler303@gmail.com>
2014-11-07 14:15:51 -0700

    FizzBuzz portion of the assignment

---
+++ Week 2 Code Challenge/Week 2 Code Challenge/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2_Code_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 20; i++)
+            {
+                FizzBuzz(i);
+            }
+        }
+        /// <summary>
+        /// Classical FizzBuzz with if's
+        /// </summary>
+        /// <param name="number">number to check</param>
+        static void FizzBuzz(int number)
+        {
+            // if the number is divisable by 5 and 3 print "FizzBuzz"
+            if (number % 5 == 0 && number % 3 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (number % 5 == 0)
+            {
+                // is divisable by 5
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 3 == 0)
+            {
+                // is divisible by 3
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                // The number is not divisible by 3 or 5 or both together, print the number
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
